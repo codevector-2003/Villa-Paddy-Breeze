d7e5a8557d90b837f7ef6b922c68ace1eca01d56
Haren Daishika <samprasharendaishika@gmail.com>
2025-10-05 13:26:05 +0530

    feat: add SpeedInsights component to main content

--- my-project/src/App.tsx
+++ my-project/src/App.tsx
@@ -64,6 +64,7 @@ export default function App() {
         <LocationSection />
         <TestimonialsSection />
         <ContactBooking />
+        <SpeedInsights />
       </main>
 
       {/* Footer */}
